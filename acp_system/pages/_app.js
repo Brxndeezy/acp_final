fb24ba24b230cc2ef0b45626a8f7eec304fd463b
Brandon Harlies <bharlies@me.com>
2022-08-31 11:21:32 +0200

    first commit

---
+++ acp_system/pages/_app.js
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import Register from './account/register'
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <Register />
+  )
+  }
+export default MyApp
---
+++ acp_system/pages/account/login.jsx
@@ -0,0 +1,12 @@
+
+import useRouter from "next/router"
+
+
+function Login(){
+    return(
+        <form action="" method="post">
+        
+
+        </form>
+    )
+}
